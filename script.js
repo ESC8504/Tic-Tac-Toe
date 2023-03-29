f0071ec3a55bcba51425c69444d74a2365994fe4
Eric Chang <fsc8504@gmail.com>
2023-03-29 15:35:18 -0700

    Basic script, style Done

--- script.js
+++ script.js
@@ -2,40 +2,38 @@ const INITIAL_MARKER = " ";
 const HUMAN_MARKER = 'X';
 const COMPUTER_MARKER = 'O';
 
-
 // different winning ways
 const WINNING_LINES = [
   [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
   [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
-  [1, 5, 9], [3, 5, 7]             // diagonals
+  [1, 5, 9], [3, 5, 7] // diagonals
 ];
 
 function initializedBoard() {
-  let board = {};
+  const board = {};
   for (let square = 1; square <= 9; square++) {
     board[String(square)] = INITIAL_MARKER;
   }
   return board;
 }
 
-
-
 function playerChoice(board) {
   document.addEventListener('DOMContentLoaded', () => {
-    let squares = document.querySelectorAll(".box");
+    const squares = document.querySelectorAll(".box");
     squares.forEach(square => {
       square.addEventListener('click', () => {
         if (square.textContent !== HUMAN_MARKER && square.textContent !== COMPUTER_MARKER) {
           square.textContent = HUMAN_MARKER;
           board[square.id] = HUMAN_MARKER;
           computerChoice(board);
+          updateScoreBoard(board);
         }
       });
     });
   });
 }
 
-function emptySquares() {
+function emptySquares(board) {
   // return an array containing the keys of the board object where the value is equal to INITIAL_MARKER.
   // In other words, is still empty
   return Object.keys(board).filter(key => board[key] === INITIAL_MARKER);
@@ -43,11 +41,10 @@ function emptySquares() {
 
 function computerChoice(board) {
   let square;
-  let center = 5;
+  const center = 5;
 
   /* The first for loop iterates through all possible winning lines, 
-    trying to find a square that would let the computer win if it played there.
-    If such a square is found, it breaks out of the loop.*/
+    trying to find a square that would let the computer win if it played there. If such a square is found, it breaks out of the loop. */
   for (let i = 0; i < WINNING_LINES.length; i++) {
     square = findAtRiskSquare(WINNING_LINES[i], board, COMPUTER_MARKER);
     if (square) break;
@@ -65,31 +62,68 @@ function computerChoice(board) {
   if (board[center] === INITIAL_MARKER) square = center;
   //If the center square is not available, the computer chooses a random empty square on the board.
   if (!square) {
-    let randomIndex = Math.floor(Math.random() * emptySquares(board).length);
+    const randomIndex = Math.floor(Math.random() * emptySquares(board).length);
     square = emptySquares(board)[randomIndex];
   }
-  //the chosen square is updated with the COMPUTER_MARKER.
+  // the chosen square is updated with the COMPUTER_MARKER.
   board[square] = COMPUTER_MARKER;
-  let squareId = document.getElementById(square.toString());
+  const squareId = document.getElementById(square.toString());
   squareId.textContent = COMPUTER_MARKER;
 }
-//possible winning combination
+// possible winning combination
 function findAtRiskSquare(line, board, marker) {
   let markersInLine = line.map(square => board[square]);
   // Check if the given marker appears twice in the line 
   if (markersInLine.filter(val => val === marker).length === 2) {
     // If it appears twice, the function tries to find the empty square in the line (
-    let unusedSquare = line.find(square => board[square] === INITIAL_MARKER);
+    const unusedSquare = line.find(square => board[square] === INITIAL_MARKER);
     if (unusedSquare !== undefined) {
       return unusedSquare;
     }
   }
-
   return null;
 }
 
-  let tttBoard = initializedBoard();
-  playerChoice(tttBoard);
+function detectWinner(board) {
+  for (let line = 0; line < WINNING_LINES.length; line++) {
+    const [sq1, sq2, sq3] = WINNING_LINES[line];
+    if (
+      board[sq1] === HUMAN_MARKER 
+      && board[sq2] === HUMAN_MARKER 
+      && board[sq3] === HUMAN_MARKER
+    ) {
+      return 'You';
+    } else if (
+      board[sq1] === COMPUTER_MARKER 
+      && board[sq2] === COMPUTER_MARKER 
+      && board[sq3] === COMPUTER_MARKER
+    ) {
+      return 'Computer';
+    }
+  }
+  return null;
+}
+
+function updateScoreBoard(board) {
+  const mainPrompt = document.querySelector(".main-prompt");
+  if (someoneWon(board)) {
+    const winner = detectWinner(board);
+    mainPrompt.textContent = `${winner} won!`;
+    // scoreBoardCount(winner, scoreBoard);
+  } else {
+    mainPrompt.textContent = `It's a tie!`;
+  }
+}
+
+function someoneWon(board) {
+  return !!detectWinner(board);
+}
+
+const tttBoard = initializedBoard();
+playerChoice(tttBoard);
+detectWinner(tttBoard);
+
+
 
  
 
